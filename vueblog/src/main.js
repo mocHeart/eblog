dcc3a1efa8b932a0f3bb0521bf8c925c9928dbbb
moc <nuc2015@163.com>
2022-05-08 21:57:49 +0800

    vue基础环境

---
+++ vueblog/src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 导入ElementUI(plus)组件
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+// 导入axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+const app = createApp(App)
+
+app.use(store)
+    .use(router)
+    .use(ElementPlus)
+    .use(VueAxios, axios)
+    .mount('#app')
+
+
+
